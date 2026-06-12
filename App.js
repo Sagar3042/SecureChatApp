5905a0468ce659da14ab1011695fff24a978599e
Sagar3042 <sagarpaul9382@gmail.com>
2026-06-12 07:04:43 +0530

    Fixed Metro Bundler crash and updated App.js

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import SecureShareScreen from './screens/SecureShareScreen';
-import FileReaderScreen from './screens/FileReaderScreen';
-import AnonymousChatScreen from './screens/AnonymousChatScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// স্ক্রিনগুলো ইম্পোর্ট করা হচ্ছে
+// সমস্ত স্ক্রিন ইম্পোর্ট করা হচ্ছে
 import LoginScreen from './screens/LoginScreen';
 import ProfileSetupScreen from './screens/ProfileSetupScreen';
+import HomeScreen from './screens/HomeScreen';
+import SecureShareScreen from './screens/SecureShareScreen';
+import FileReaderScreen from './screens/FileReaderScreen';
+import AnonymousChatScreen from './screens/AnonymousChatScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,31 +16,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen 
-          name="Login" 
-          component={LoginScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="ProfileSetup" 
-          component={ProfileSetupScreen} 
-          options={{ title: 'প্রোফাইল সেটআপ' }} 
-        />
-        <Stack.Screen 
-  name="FileReader" 
-  component={FileReaderScreen} 
-  options={{ title: 'File Reader & Browser' }} 
-/>
-        <Stack.Screen 
-  name="SecureShare" 
-  component={SecureShareScreen} 
-  options={{ title: 'Secure Share' }} 
-/>
-<Stack.Screen 
-  name="AnonymousChat" 
-  component={AnonymousChatScreen} 
-  options={{ title: 'Anonymous Inbox' }} 
-/>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ProfileSetup" component={ProfileSetupScreen} options={{ title: 'প্রোফাইল সেটআপ' }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="SecureShare" component={SecureShareScreen} options={{ title: 'Secure Share' }} />
+        <Stack.Screen name="FileReader" component={FileReaderScreen} options={{ title: 'File Reader' }} />
+        <Stack.Screen name="AnonymousChat" component={AnonymousChatScreen} options={{ title: 'Anonymous Inbox' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
